efb7f76805ff6d7237bf16347c662e750da7c6dd
Patryk <krasowski.p@outlook.com>
2024-02-09 21:23:30 +0100

    added button new column to tasks, add hover animation

--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -11,7 +11,9 @@ export default function TaskList({ getTaskInfo, className }) {
 
 	if (currentBoard) {
 		return (
-			<main className={`flex px-1.6 gap-x-2.4 py-2.4 snap-x overflow-y-auto lg:overflow-x-hidden  ${className}`}>
+			<main
+				className={`flex px-1.6 gap-x-2.4 py-2.4 snap-x overflow-y-auto lg:overflow-x-hidden tablet:h-full  ${className}`}
+			>
 				{currentBoard.columns.map((board, id) => {
 					let color
 					if (id === 0) color = 'bg-blue'
@@ -20,13 +22,13 @@ export default function TaskList({ getTaskInfo, className }) {
 					if (id > 2) color = 'bg-yellow'
 					return (
 						<ul key={board.name} className='min-w-28  max-w-28'>
-							<li className='flex mb-2.4 text-hs text-medium-grey uppercase'>
+							<li className='flex mb-2.4 text-hs text-medium-grey uppercase '>
 								<div className={`w-1.5 h-1.5 ${color} rounded-full mr-1.2`}></div> {board.name} (
 								{board.tasks?.length ?? '0'})
 							</li>
 							{board.tasks?.map(task => {
 								return (
-									<li key={task.title} className=' mb-2  drop-shadow-md  bg-white dark:bg-dark-grey rounded-0.8 '>
+									<li key={task.title} className=' mb-2  drop-shadow-md  bg-white dark:bg-dark-grey rounded-0.8 hover:scale-95 transition-transform'>
 										<Button
 											className='px-1.6 py-2.3 text-left'
 											onClick={() => {
@@ -42,6 +44,11 @@ export default function TaskList({ getTaskInfo, className }) {
 						</ul>
 					)
 				})}
+				<Button
+					className={`min-w-28  max-w-28 mt-3.94 text-hxl dark:text-medium-grey bg-medium-grey dark:bg-dark-grey-opacity hover:scale-95 transition-transform rounded-0.6 `}
+				>
+					+ New Column
+				</Button>
 				<Button
 					className={`flex ${
 						visualCtx.showSidebar ? 'left-0 opacity-100 delay-300' : 'left-minus5.6 opacity-0 '
